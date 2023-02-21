5120dc7f864f3d354f6b1f613797e6c56c05d559
Ismail Muhammad Adam <61629819+softiey@users.noreply.github.com>
2023-02-22 00:28:48 +0100

    Add files via upload

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print Holberton, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('\n');
+
+	return (0);
+}
