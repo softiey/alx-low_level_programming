041d636c0443c5d3a257fe3a72cd170dbbb92c44
softiey <abu_nabeel@yahoo.com>
2023-02-27 14:19:43 -0800

    auto

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * The program generate ramdompassword 
+ * to crack 
+ * @password hfhfj
+ * @charset gdgygfdb
+ * @stand
+ */
+
+#define PASSWORD_LENGTH 10
+
+int main() {
+  char password[PASSWORD_LENGTH + 1]; /* +1 for null terminator*/
+  const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  srand(time(NULL));
+
+  for (int i = 0; i < PASSWORD_LENGTH; i++) {
+    password[i] = charset[rand() % sizeof(charset)];
+  }
+
+  password[PASSWORD_LENGTH] = '\0';
+
+  printf("Generated password: %s\n", password);
+
+  return 0;
+}
